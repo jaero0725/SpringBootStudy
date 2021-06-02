6eabbf1f907007a861684e864f2ee75964f5a877
choijaeho <dhqkgbs@naver.com>
2021-06-02 17:09:34 +0900

    [INITIAL] helloSpringSecurity 생성

---
+++ hellospringsecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/HellospringsecurityApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.hellospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringsecurityApplication.class, args);
+    }
+
+}
---
+++ hellospringsecurity/src/test/java/kr/ac/hansung/cse/hellospringsecurity/HellospringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.hellospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
