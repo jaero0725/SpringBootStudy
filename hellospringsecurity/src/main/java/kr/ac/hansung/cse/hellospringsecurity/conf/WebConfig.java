8621680039d8480dc5232a04e5774659bc1e61b5
choijaeho <dhqkgbs@naver.com>
2021-06-03 15:15:51 +0900

    [UPDATE] 부트스트랩 적용

--- hellospringsecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/conf/WebConfig.java
+++ hellospringsecurity/src/main/java/kr/ac/hansung/cse/hellospringsecurity/conf/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry)   {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("login-bootstrap");
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/admin/home").setViewName("adminhome");
         registry.addViewController("/accessDenied").setViewName("403");
